7991c0a64c30d0d74ae8d5527a6aaabbcfa1ecf3
Eedriz29 <idrismorakinyo29@gmail.com>
2026-08-02 14:49:45 +1000

    fmt done

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -141,22 +141,22 @@ resource "aws_eks_access_policy_association" "ci_user_admin" {
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name  = aws_eks_cluster.starttech-cluster.name
-  addon_name    = "vpc-cni"
+  cluster_name                = aws_eks_cluster.starttech-cluster.name
+  addon_name                  = "vpc-cni"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name  = aws_eks_cluster.starttech-cluster.name
-  addon_name    = "kube-proxy"
+  cluster_name                = aws_eks_cluster.starttech-cluster.name
+  addon_name                  = "kube-proxy"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name  = aws_eks_cluster.starttech-cluster.name
-  addon_name    = "coredns"
+  cluster_name                = aws_eks_cluster.starttech-cluster.name
+  addon_name                  = "coredns"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
